294e8629a81ddd9d5c0b1d63892fcdc02ef89615
iamtaufik <muhtopik07@gmail.com>
2023-03-20 14:27:24 +0700

    add endpoint to get single vote

--- src/pages/Participant.tsx
+++ src/pages/Participant.tsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { FormEvent, useState } from 'react';
+import { toast } from 'react-toastify';
+import workTogetherBg from '../assets/work-together.svg';
 
 const Participant = () => {
+  const [code, setCode] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      await axios.get(`http://localhost:3000/api/votes/${code}`);
+
+      navigate(`/participant/${code}`);
+    } catch (error: any) {
+      if (error.response?.data.code === 404) {
+        toast.error('Hmm kode voting tidak ditemukan', {
+          position: 'top-right',
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: 'colored',
+        });
+      }
+      console.error(error);
+    }
+  };
+
   return (
-    <div>
-      {/* Create input code for join voteing */}
-      Participant
+    <div className=" flex justify-center items-center flex-col">
+      <img src={workTogetherBg} alt="Work Together " className="w-[300px] h-[300px]" />
+      <h1>Ikut Voting</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="code" id="code" placeholder="Kode Voting" onChange={(e) => setCode(e.target.value)} />
+        <button type="submit">Gabung</button>
+      </form>
     </div>
   );
 };
--- src/pages/Vote.tsx
+++ src/pages/Vote.tsx
@@ -1,8 +1,38 @@
-import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { Votes } from '../types/votes';
 
 const Vote = () => {
+  const [vote, setVote] = useState<Votes>();
   const { code } = useParams();
-  return <div>{code}</div>;
+  const navigate = useNavigate();
+
+  const getVote = async (code: string) => {
+    try {
+      const { data } = await axios.get(`http://localhost:3000/api/votes/${code}`);
+      setVote(data.result);
+    } catch (error: any) {
+      if (error.response?.data.code === 404) {
+        navigate('/');
+      }
+      console.error(error.message);
+    }
+  };
+  useEffect(() => {
+    getVote(String(code));
+  }, []);
+
+  return (
+    <div>
+      {vote?.candidates.map((c, index) => (
+        <div key={index}>
+          <p>{c.name}</p>
+          <p>{c.votes}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Vote;
